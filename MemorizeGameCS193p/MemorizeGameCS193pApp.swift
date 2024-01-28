1f1343fe3e5f0f58bfe5c58d9d40347e3a1fa1a4
mia <miabenn74@gmail.com>
2024-01-28 19:51:54 +1000

    Initial Commit

---
+++ MemorizeGameCS193p/MemorizeGameCS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameCS193pApp.swift
+//  MemorizeGameCS193p
+//
+//  Created by Mia on 28/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameCS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
